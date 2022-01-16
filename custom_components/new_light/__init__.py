36ab31a8845896b05727752261ebdc1ee177f121
alxld <aaronandlouise@yahoo.com>
2022-01-16 04:21:34 +0000

    Added setup to init

--- custom_components/new_light/__init__.py
+++ custom_components/new_light/__init__.py
@@ -1,4 +1,19 @@
 """The new_light integration."""
+from __future__ import annotations
+
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.typing import ConfigType
+
+DOMAIN = "new_light"
+
+
+def setup(hass: HomeAssistant, config: ConfigType) -> bool:
+    """Your controller/hub specific code."""
+    hass.helpers.discovery.load_platform("light", DOMAIN, {}, config)
+
+    return True
+
+
 # from __future__ import annotations
 #
 # from homeassistant.config_entries import ConfigEntry
